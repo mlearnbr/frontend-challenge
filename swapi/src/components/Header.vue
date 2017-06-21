8150982302335813bc03b690b38cf73c8d5ce8a2
Augusto Soares <gutosoares@outlook.com>
2017-06-21 11:26:08 -0300

    Edit header component

--- swapi/src/components/Header.vue
+++ swapi/src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <header>
     <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">SW API</a>
+      <div class="nav-wrapper container">
+        <router-link to="/" class="brand-logo center hide-on-med-and-down"><i class="material-icons">stars</i> SW API</router-link>
         <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><a href="sass.html">Peoples</a></li>
-          <li><a href="badges.html">Planets</a></li>
+          <li><router-link to="/people">Peoples</router-link></li>
+          <li><router-link to="/planets">Planets</router-link></li>
         </ul>
+        <div class="menu-mobile hide-on-large-only">
+          <router-link to="/" class="brand-logo left"><i class="material-icons">stars</i> SW API</router-link>
+          <ul class="right hide-on-large-only">
+            <li><router-link to="/people">People</router-link></li>
+            <li><router-link to="/planets">Planets</router-link></li>
+          </ul>
+        </div>
       </div>
     </nav>
   </header>
@@ -17,3 +24,18 @@ export default {
   name: 'header'
 }
 </script>
+
+<style>
+header nav {
+  background-color: #13293D;
+}
+
+header nav .brand-logo {
+  color: #1de9c3;
+  font-weight: 200;
+}
+
+.menu-mobile ul {
+  margin-left: 40px;
+}
+</style>
