5d45bb71ba4f9f86a7bde39182207e4ec0c04a0e
Augusto Soares <gutosoares@outlook.com>
2017-06-21 12:37:57 -0300

    Edit components

--- swapi/src/components/People.vue
+++ swapi/src/components/People.vue
@@ -4,11 +4,12 @@
     <div class="row">
       <div class="col xl12 l12 m12 s12">
         <div class="row">
-          <div class="col s6" v-for="(person, index) in peopleList">
+          <div class="col s6" v-for="(person, key, index) in peopleList">
             <ul class="collection">
               <li class="collection-item avatar indigo lighten-5">
                 <i class="material-icons circle">person_pin</i>
                 <span class="title">{{ person.name }}</span>
+                <p>Specie: {{ speciesList.name }}</p>
                 <p>Birth Year: {{ person.birth_year }}</p>
                 <router-link :to="'/people/' + index" class="secondary-content"><i class="material-icons">grade</i></router-link>
               </li>
@@ -25,20 +26,21 @@ export default {
   name: 'people',
   data() {
     return {
-      peopleList: []
+      peopleList: [],
+      speciesList: []
     }
   },
   methods: {
     fetchApi() {
       this.$http.get('http://swapi.co/api/people').then(response => {
         this.peopleList = response.data.results
-        console.log(response)
-        console.log(this.peopleList)
       })
     },
 
     getSpecies() {
-      this.$http.get('http://swapi.co/api/species').then()
+      this.$http.get('http://swapi.co/api/species').then(response => {
+        this.speciesList = response.data.results
+      })
     }
   },
   created() {
--- swapi/src/components/PersonDetails.vue
+++ swapi/src/components/PersonDetails.vue
@@ -1,8 +1,38 @@
 <template>
-
+  <section class="details container">
+  <h1>Person details</h1>
+    <div class="row">
+      <div class="col s6">
+        <ul class="collection">
+          <li class="collection-item avatar indigo lighten-5">
+            <i class="material-icons circle">person_pin</i>
+            <span class="title">{{ personDetails.name }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+export default {
+  name: 'details',
+  data() {
+    return {
+      personDetails: {}
+    }
+  },
+  methods: {
+    fetchSinglePerson(id) {
+      this.$http.get('http://swapi.co/api/people/' + id).then(response => {
+        console.log(response)
+      })
+    }
+  },
+  created() {
+    this.fetchSinglePerson(this.$route.params.id)
+  }
+}
 </script>
 
 <style lang="scss">
