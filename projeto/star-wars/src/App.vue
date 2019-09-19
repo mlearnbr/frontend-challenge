83a27b1612d3ad281e01062767bf3ca26519fe91
Euler Amorim <euler.amorim@hotmail.com>
2019-09-19 06:15:28 -0300

    Montando layout da tela e configurando rotas e estilos

--- projeto/star-wars/src/App.vue
+++ projeto/star-wars/src/App.vue
@@ -1,28 +1,34 @@
-<template>
+<template>  
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <starWars/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import starWars from './components/starWars';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App',
+  components: {    
+    starWars
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
