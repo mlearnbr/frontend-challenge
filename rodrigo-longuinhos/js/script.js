a82f37c8ffbdea5774774efc6e958f7c5c22b81e
rodrigoassisnz <rodrigoassisnz@hotmail.com>
2020-03-04 16:49:52 -0300

    Mudança de função para buscar o JSON
      $.getJSON(url, function (cardStarWars) {}

--- rodrigo-longuinhos/js/script.js
+++ rodrigo-longuinhos/js/script.js
@@ -1,27 +1,15 @@
-$(document).ready(function () {
-    var urlStr = "https://swapi.co/api/people/1/";
-    
-    $.ajax({
-        url: urlStr,
-        type: "get",
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-
-            $("txtName").val(data.name);
-            $("txtBirth_year").val(data.birth_year);
-            $("txtEye_color").val(data.eye_color);
-            $("txtGender").val(data.gender);
-            $("txtHair_color").val(data.hair_color);
-            $("txtHeight").val(data.height);
-            $("txtMass").val(data.mass);
-            $("txtSkin_color").val(data.skin_color);
-            $("txtHomeworld").val(data.homeworld);
-            $("txtFilms").val(data.films);
-            $("txtSpecies").val(data.species);
-        },
-        error: function (erro) {
-            console.log(erro);
-        }
-    });
-});
+let url = 'https://swapi.co/api/people/1/';
+$.getJSON(url, function (cardStarWars) {
+    console.log(cardStarWars.name);
+    $("input:nth-child(1)").attr('value', cardStarWars.name);
+    $("input:nth-child(2)").attr('value', cardStarWars.birth_year);
+    $('input:nth-child(3)').attr('value',cardStarWars.eye_color);
+    $('input:nth-child(4)').attr('value',cardStarWars.gender);
+    $('input:nth-child(5)').attr('value',cardStarWars.hair_color);
+    $('input:nth-child(6)').attr('value',cardStarWars.height);
+    $('input:nth-child(7)').attr('value',cardStarWars.mass);
+    $('input:nth-child(8)').attr('value',cardStarWars.skin_color);
+    $('input:nth-child(9)').attr('value',cardStarWars.homeworld);
+    $('input:nth-child(10)').attr('value',...cardStarWars.films);
+    $('input:nth-child(11)').attr('value',cardStarWars.species);
+})
