918a761f2570ca0d31bba8f46175b50c0bbf13d4
Calebe Liberio <calebe.pedroso@paiva.com.br>
2021-11-20 01:46:10 -0300

    Create object People

---
+++ app/src/entities/People.js
@@ -0,0 +1,21 @@
+class People {
+  constructor(
+    name,
+    birth_year,
+    eye_color,
+    gender,
+    hair_color,
+    height,
+    mass,
+    skin_color,
+    homeworld,
+    films,
+    species
+  ) {
+    this.name = name;
+    this.birth_year = birth_year;
+    this.gender = gender;
+  }
+}
+
+export default People;
