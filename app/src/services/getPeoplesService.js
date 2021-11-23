d88bd779dd4281e82e1b9a79f3e0e92a7fa3be38
Calebe Liberio <calebe.pedroso@paiva.com.br>
2021-11-22 23:29:23 -0300

    Change people services

--- app/src/services/getPeoplesService.js
+++ app/src/services/getPeoplesService.js
@@ -1,17 +1,36 @@
 import People from "../entities/People";
 
 import getPeople from "../repositories/getPeoplesRepository";
+import getSpeciesService from "../services/getSpeciesService";
+import getFilmsServices from "../services/getFilmsServices";
+import getPlanetsServices from "./getPlanetsServices";
 
 const getPeopleService = async () => {
+  const speciesList = await getSpeciesService();
+  const species = speciesList ? speciesList : [];
+
+  const filmsList = await getFilmsServices();
+  const films = filmsList ? filmsList : [];
+
+  const planetsList = await getPlanetsServices();
+  const planets = planetsList ? planetsList : [];
+
   const repositoryResponse = await getPeople();
   if (repositoryResponse.status) {
-    return setPeople(repositoryResponse.data);
+    return setPeople(repositoryResponse.data, species, planets, films);
   }
 
   return { message: "Failed to make the request." };
 };
 
-function setPeople(peoples) {
+function getFilmPeople(film, peopleFilms) {
+  const arrayFilms = [];
+  return arrayFilms.push(
+    peopleFilms.filter((peopleFilm) => peopleFilm === film)
+  );
+}
+
+function setPeople(peoples, species, planets, films) {
   const objectPeoples = peoples.results.map(
     (people) =>
       new People(
@@ -24,11 +43,11 @@ function setPeople(peoples) {
         people.mass,
         people.skin_color,
         people.homeworld,
-        people.films,
-        people.species
+        films.filter((film) => getFilmPeople(film, people.films)),
+        species.filter((specie) => people.species[0] === specie.url),
+        planets.filter((planet) => people.homeworld === planet.urlPlanet)
       )
   );
-  console.log(objectPeoples);
   return objectPeoples;
 }
 
