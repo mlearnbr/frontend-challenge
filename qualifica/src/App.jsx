504fd11473aec416e77b3460a8065aac7928971a
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-08-07 16:42:18 -0300

    feat: adjusting routes

---
+++ qualifica/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+
+import "./global.css";
+import Routes from "./routes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
