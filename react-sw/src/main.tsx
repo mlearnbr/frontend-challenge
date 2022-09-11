c79d4d4c1fdb1393e287b6c91b572001056f88e2
CoutoDev <c.svitor@yahoo.com.br>
2022-09-11 20:09:07 -0300

    feat: create context and apply to main

--- react-sw/src/main.tsx
+++ react-sw/src/main.tsx
@@ -7,14 +7,17 @@ import {
   Route
 } from "react-router-dom"
 import CharacterPage from './components/CharacterPage'
+import CharacterProvider from './contexts/CharacterContex'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/person' element={<CharacterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CharacterProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/person' element={<CharacterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CharacterProvider>
   </React.StrictMode>
 )
