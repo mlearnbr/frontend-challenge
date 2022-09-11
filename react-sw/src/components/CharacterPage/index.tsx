d449a5f0b4d21eff3ae28ebcef9da99eb1bb740b
CoutoDev <c.svitor@yahoo.com.br>
2022-09-11 17:03:21 -0300

    feat: initial char page

--- react-sw/src/components/CharacterPage/index.tsx
+++ react-sw/src/components/CharacterPage/index.tsx
@@ -2,7 +2,24 @@ function CharactersPage() {
 
   return (
     <>
-      Page
+      <section>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <details>
+          <p>name</p>
+          <p>birth_year</p>
+          <p>eye_color</p>
+          <p>gender</p>
+          <p>hair_color</p>
+          <p>height</p>
+          <p>mass</p>
+          <p>skin_color</p>
+          <p>homeworld</p>
+          <p>filmes</p>
+          <p>species</p>
+        </details>
+      </section>
     </>
   )
 }
