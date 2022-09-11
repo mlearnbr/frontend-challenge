c8beaad6d4d11241a8a62c2710913140216927f7
CoutoDev <c.svitor@yahoo.com.br>
2022-09-11 17:02:39 -0300

    feat: cache thubms with localstorage

--- react-sw/src/services/thumbnails-api.ts
+++ react-sw/src/services/thumbnails-api.ts
@@ -2,7 +2,14 @@ import axios from 'axios'
 import { CharacterThumbnail } from '../typings/characters'
 const thumbnailsAPI = {
   getCharacterThumbnail: (character: string): Promise<CharacterThumbnail> => {
+    const cachedThumb = localStorage.getItem(`${character}-thumb`)
+    if(cachedThumb) {
+      return Promise.resolve({
+        thumbnail: cachedThumb
+      }) 
+    }
     return axios.get(`http://localhost:3333/character-thumbnail/${character}`).then((res) => {
+      localStorage.setItem(`${character}-thumb`, res.data?.thumbnail)
       return res.data
     })
   }
