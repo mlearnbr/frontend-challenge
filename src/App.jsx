82b13107ddec23e6c91f67fec4dcbf78ac26559c
eduardojigub <eduardobrunoit@gmail.com>
2022-10-14 01:23:07 -0300

    feat: creating page/components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/:characterId" element={<Details />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
