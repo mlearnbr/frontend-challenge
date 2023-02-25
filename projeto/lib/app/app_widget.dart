e7c62f006de7db58f4c8977d87cc3524d56f7151
Eduarda Lima <eududalim@gmail.com>
2023-02-25 15:35:46 -0300

    created routes nominated

--- projeto/lib/app/app_widget.dart
+++ projeto/lib/app/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:star_wars_app/app/layers/domain/models/person_model.dart';
+import 'package:star_wars_app/app/layers/presenters/view/details/details_page.dart';
+import 'package:star_wars_app/app/layers/presenters/view/home/home_page.dart';
 
 import 'shared/theme/theme.dart';
 
@@ -12,11 +15,48 @@ class MyApp extends StatelessWidget {
       title: 'Persons Star Wars',
       theme: DefaultTheme.theme,
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Material(),
-        '/home': (context) => const Scaffold(),
+      initialRoute: HomePage.route,
+      onGenerateRoute: (settings) {
+        var route = settings.name;
+        Widget page;
+
+        switch (route) {
+          case DetailsPage.route:
+            page = DetailsPage();
+            break;
+
+          case HomePage.route:
+            page = const HomePage();
+            break;
+
+          default:
+            page = const HomePage();
+        }
+
+        return PageRouteBuilder(
+            maintainState: false,
+            settings: settings,
+            pageBuilder: (context, animation, secondaryAnimation) => page,
+            transitionDuration: const Duration(milliseconds: 200),
+            transitionsBuilder: (context, animation, secAnimation, child) {
+              animation =
+                  CurvedAnimation(parent: animation, curve: Curves.decelerate);
+              return SlideTransition(
+                position: Tween(
+                        begin: const Offset(1.0, 0.0),
+                        end: const Offset(0.0, 0.0))
+                    .animate(animation),
+                child: child,
+              );
+            });
+        /* 
+              return MaterialPageRoute(
+                  builder: (context) => page, settings: settings); */
       },
+      // routes: {
+      //   HomePage.route: (context) => const HomePage(),
+      //   DetailsPage.route: (context) => DetailsPage(),
+      // },
     );
   }
 }
