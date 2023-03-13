2accfac819902e940924ec8a241e2c64d93e8297
jaoFranca <joaovfranca1@gmail.com>
2023-03-13 15:01:43 -0300

    created the http adapter

---
+++ lib/Core/data/http/http_client.dart
@@ -0,0 +1,7 @@
+abstract class HttpClient {
+  Future<Map?> request({
+    required String url,
+    required String method,
+    Map body,
+  });
+}
---
+++ lib/Core/infra/http/http_adapter.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+  HttpAdapter(
+    this.client,
+  );
+
+  @override
+  Future<Map?> request({
+    required String url,
+    required String method,
+    Map? body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    var response = Response('', 500);
+    try {
+      if (method == 'post') {
+        response = await client.post(
+          Uri.parse(url),
+          headers: headers,
+          body: jsonBody,
+        );
+      }
+      if (method == 'get') {
+        response = await client.get(
+          Uri.parse(url),
+          headers: headers,
+        );
+      }
+    } catch (error) {
+      throw HttpError.serverError;
+    }
+
+    return _handleResponse(response);
+  }
+
+  Map? _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else if (response.statusCode == 400) {
+      throw HttpError.badRequest;
+    } else if (response.statusCode == 401) {
+      throw HttpError.unauthorized;
+    } else if (response.statusCode == 403) {
+      throw HttpError.forbidden;
+    } else if (response.statusCode == 404) {
+      throw HttpError.notFound;
+    } else {
+      throw HttpError.serverError;
+    }
+  }
+}
